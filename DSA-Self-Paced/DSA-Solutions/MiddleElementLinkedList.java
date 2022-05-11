e554e1a98f4b0366c89d0eb2ae5dd5ab2668b4dd
Drupad Preenja <drupad.preenja1@gmail.com>
2022-05-11 20:02:01 +0530

    Middle Element in Linked List Changes

--- DSA-Self-Paced/DSA-Solutions/MiddleElementLinkedList.java
+++ DSA-Self-Paced/DSA-Solutions/MiddleElementLinkedList.java
@@ -12,19 +12,26 @@ public class MiddleElementLinkedList {
         head.next.next.next.next = new Node(50);
         head.next.next.next.next.next = new Node(60);
         Node result = middleElement(head);
-        if(result != null){
+        if (result != null) {
             System.out.println(result.data);
-        }else{
+        } else {
             System.out.println(result);
         }
     }
 
+    /**
+     * Method to find the Middle elemnt of the linked list
+     * In case of even length it will print the right element
+     * 
+     * @param head
+     * @return
+     */
     private static Node middleElement(Node head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return head;
         }
         Node first = head, second = head;
-        while(first != null && first.next != null){
+        while (first != null && first.next != null) {
             second = second.next;
             first = first.next.next;
         }
